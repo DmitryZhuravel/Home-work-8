55932166bc85d681ac26b2e908e68b58ac5433c1
«Dmitry» <d_m_i_t_r_I_I@mail.ru>
2022-04-06 14:54:59 +0500

    Трехмерный массив и его построчный вывод с указанием элементов

---
+++ HW4/Program.cs
@@ -0,0 +1,73 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся 
+// двузначных чисел. Напишите программу, которая будет 
+// построчно выводить массив, добавляя индексы каждого элемента.
+int[,,] myArray= MyArray(2, 5, 5);
+PrintMyArray(myArray);
+Console.WriteLine("Трехмерный массив:");
+PrintRowbyRowArray(myArray);
+
+int GenerateUniqueNumber()
+{
+    HashSet<int> numbers = new HashSet<int>();;
+    Random rnd = new Random();
+    while (true)
+    {
+        int n = rnd.Next(10, 100);
+        if (!numbers.Contains(n))
+        {
+            numbers.Add(n);
+            return n;
+        }
+    }
+}
+
+int[,,] MyArray(int row, int column, int plane)
+{
+    int[,,] myArray = new int[row, column, plane];
+    Random rnd = new Random();
+    for (int i = 0; i < myArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < myArray.GetLength(1); j++)
+        {
+            for (int k = 0; k < myArray.GetLength(2); k++)
+            {
+                myArray[i, j, k] =GenerateUniqueNumber();
+            }  
+        }
+    }
+    return myArray;
+}
+
+void PrintMyArray(int[,,] array)
+{
+    for (int i = 0; i < myArray.GetLength(0); i++)
+    {
+        Console.WriteLine("=== Page N: " + (i + 1)+" ===");
+        for (int j = 0; j < myArray.GetLength(1); j++)
+        {
+            for (int k = 0; k < myArray.GetLength(2); k++)
+            {
+                Console.Write($"{myArray[i, j, k]}\t");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine(" ");
+    }
+}
+
+void PrintRowbyRowArray(int[,,] array)
+{               
+    for (int i = 0; i < myArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < myArray.GetLength(1); j++)
+        {
+            for (int k = 0; k < myArray.GetLength(2); k++)
+            {
+               Console.WriteLine($" Элемент трехмерного массива с индексом [ {i}, {j}, {k}] имеет значение {myArray[i,j,k]} "); 
+            }
+            
+        }
+    }
+}
+
+
